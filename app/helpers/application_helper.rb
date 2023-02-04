0f303d6a080f98e5cac0d18ca7005fba112e2cb4
Arne De Herdt <arne.deherdt@customink.com>
2023-02-04 12:30:19 +0100

    [SETUP] Development & Test environment
    This pull request simply sets up the basic environment for development and testing.
    By ensuring all required gems are available, and the configuration has been done for them,
    we can have future additions adhere to our coding standards.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# This class is the base view helper that is available to every single view in the application.
+# Shared functionality can be defined here, but extending this module has a performance impact overall
+# on the application as the module will be required everywhere.
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
+# The ApplicationRecord class forms the basis for all models and allows them to communicate with the database.
+# Shared logic for any model can be defined in this class, as long as the intend of keeping models thing is respected.
+# A Models primary concern is communicating with the database and ensuring that the data is represented correctly.
+#
+# Validations are normally defined on Models in the Ruby on Rails framework, but to keep the code lean and simple,
+# dedicated validators will be created in the *validators* namespace, allowing them to focus on verifying the logic
+# and simply using the actual models to retrieve the data as requested.
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
